0ed72e65ff66a7dc7f9d2159bf9cabc4deb24af1
omararra <omarsamirelmokadem@gmail.com>
2024-10-27 18:05:48 +0300

    Updated the main function file for it to work

--- function_app.py
+++ function_app.py
@@ -1,27 +1,12 @@
-import azure.functions as func
 import logging
-import qatarsale
+import azure.functions as func
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+app = func.FunctionApp()
 
-@app.route(route="httptrig")
-def httptrig(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+@app.schedule(schedule="0/5 * * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False)
+#@app.queue_output(arg_name="msg", queue_name="outqueue", connection="AzureWebJobsStorage")
+def timer_trigger(myTimer: func.TimerRequest) -> None:
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
 
-    if name:
-        print("Starting qatarsale.py")
-        qatarsale.main()
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    logging.info('Python timer trigger function executed.')
