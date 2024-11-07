c1202bc75b1e582efc1ada2ee3953d16f44f14c8
omararra <omarsamirelmokadem@gmail.com>
2024-11-07 18:17:39 +0300

    works

--- qatarsale.py
+++ qatarsale.py
@@ -4,8 +4,8 @@ import extract_details as ext
 # Function to send WhatsApp message with car details
 def send_whatsapp_message(car_details):
     print("Sending WhatsApp message...")
-    account_sid = 'ACca76c3474f8f63f7d0cb40df8a3b4082'
-    auth_token = '6bd3945c890e12a296409b2f9ad15fa3'
+    account_sid = 'ACcc19f7f4b5e9972e26f19afc773a076a'
+    auth_token = '9c901fee2bb955dd749acc740f2466d2'
     client = Client(account_sid, auth_token)
     
     message = client.messages.create(
@@ -22,8 +22,8 @@ def send_whatsapp_message(car_details):
             f"Mileage: {car_details[8]}\n"
             f"Link: {car_details[9]}"
         ),
-        from_='whatsapp:+14155238886',
-        to='whatsapp:+97455741660',
+        from_='whatsapp:+97477086807',
+        to='whatsapp:+97455340079',
     )
     print("Message sent:", message.sid)
 def main(previous):
