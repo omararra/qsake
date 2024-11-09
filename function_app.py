75d4e5d7db72e2b136d1f34ed8628c1985d65dae
omararra <omarsamirelmokadem@gmail.com>
2024-11-10 00:24:42 +0300

    added email and discord support and removed whatsapp

--- function_app.py
+++ function_app.py
@@ -5,21 +5,33 @@ import extract_details as ext
 import time
 
 app = func.FunctionApp()
-@app.schedule(schedule="* * * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False)
+car_listings = ext.get_car_listings(1)
+
+with open("cars.txt", "w") as f:
+    for car in car_listings:
+        f.write(f"{car}\n")
+@app.schedule(schedule="*/30 * * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False)
 #@app.queue_output(arg_name="msg", queue_name="outqueue", connection="AzureWebJobsStorage")
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
-    print("Fetching current car listings...")
+    logging.info("Fetching current car listings...")
     car_listings = ext.get_car_listings(1)
-    print("Returning current listings")
-    print("Sleeping for 10 seconds")
-    seconds = 10
-    while seconds > 0:
-        print(f"Time left: {seconds} seconds")
-        time.sleep(1)  # Wait for 1 second
-        seconds -= 1
-    print("Calling main function...")
-    qs.main(car_listings)
-    print("Exiting main function")
+    # def convert_type(value): 
+    #     try: 
+    #         return eval(value) 
+    #     except: 
+    #         return value 
+    previous = []
+    with open("cars.txt", "r") as file: 
+        for line in file:
+            previous.append(eval(line.strip()))
+    if car_listings == previous:
+        logging.info("No new cars")
+    else:
+        current = qs.main(car_listings, previous)
+        for car in current:
+            with open("cars.txt", "w") as f:
+                f.write(f"{car}\n")
+    logging.info("Exiting main function")
     logging.info('Python timer trigger function executed.')
--- qatarsale.py
+++ qatarsale.py
@@ -1,15 +1,13 @@
-from twilio.rest import Client
-import extract_details as ext
+import requests
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
-# Function to send WhatsApp message with car details
-def send_whatsapp_message(car_details):
-    print("Sending WhatsApp message...")
-    account_sid = 'ACcc19f7f4b5e9972e26f19afc773a076a'
-    auth_token = '9c901fee2bb955dd749acc740f2466d2'
-    client = Client(account_sid, auth_token)
-    
-    message = client.messages.create(
-        body=(
+def send_message(car_details):
+    print("Sending Discord message...")
+    webhook_url = "https://discord.com/api/webhooks/1304917955603071066/X-m3t_bmut_ZMr7eh5_9NCbTlPXid2sWIevcBfiT0x014B6NLKXRpV7fmxkoOzhjy4zT"
+    data = {
+        "content": (
             f"New car listing:\n"
             f"Make: {car_details[0]}\n"
             f"Model: {car_details[1]}\n"
@@ -20,24 +18,72 @@ def send_whatsapp_message(car_details):
             f"Gear Type: {car_details[6]}\n"
             f"Cylinders: {car_details[7]}\n"
             f"Mileage: {car_details[8]}\n"
-            f"Link: {car_details[9]}"
-        ),
-        from_='whatsapp:+97477086807',
-        to='whatsapp:+97455340079',
-    )
-    print("Message sent:", message.sid)
-def main(previous):
-    current = ext.get_car_listings(1)
-    print("Fetching current car listings...")
+            f"Link: <{car_details[9]}>\n"
+            f"Image: {car_details[10]}"
+        )
+    }
+    response = requests.post(webhook_url, json=data)
+    if response.status_code == 204:
+        print("Discord message sent successfully.")
+    else:
+        print(f"Failed to send message. Status code: {response.status_code}")
+    print("Sending Email message...")
+    message = Mail(
+        from_email='info@oryxdev.tech',
+        to_emails='smokadem@gmail.com',
+        subject='New Listing',
+        html_content=(
+            f"New car listing:<br>"
+            f"<strong>Make:</strong> {car_details[0]}<br>"
+            f"<strong>Model:</strong> {car_details[1]}<br>"
+            f"<strong>Submodel:</strong> {car_details[2]}<br>"
+            f"<strong>Price:</strong> {car_details[3]} QAR<br>"
+            f"<strong>Type:</strong> {car_details[4]}<br>"
+            f"<strong>Year:</strong> {car_details[5]}<br>"
+            f"<strong>Gear Type:</strong> {car_details[6]}<br>"
+            f"<strong>Cylinders:</strong> {car_details[7]}<br>"
+            f"<strong>Mileage:</strong> {car_details[8]}<br>"
+            f"<strong>Link:</strong> <a href='{car_details[9]}'>Click Here</a><br>"
+        ))
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+    except Exception as e:
+        print(e.message)
+    # print("Sending WhatsApp message...")
+    # account_sid = 'ACcc19f7f4b5e9972e26f19afc773a076a'
+    # auth_token = '9c901fee2bb955dd749acc740f2466d2'
+    # client = Client(account_sid, auth_token)
     
+    # message = client.messages.create(
+    #     body=(
+    #         f"New car listing:\n"
+    #         f"Make: {car_details[0]}\n"
+    #         f"Model: {car_details[1]}\n"
+    #         f"Submodel: {car_details[2]}\n"
+    #         f"Price: {car_details[3]} QAR\n"
+    #         f"Type: {car_details[4]}\n"
+    #         f"Year: {car_details[5]}\n"
+    #         f"Gear Type: {car_details[6]}\n"
+    #         f"Cylinders: {car_details[7]}\n"
+    #         f"Mileage: {car_details[8]}\n"
+    #         f"Link: {car_details[9]}"
+    #     ),
+    #     from_='whatsapp:+97477086807',
+    #     to='whatsapp:+97455741660',
+    # )
+    # print("Whatsapp message sent:", message.sid)
+def main(current, previous):
     new_listings = [car for car in current if car not in previous]
     print(f"New listings: {len(new_listings)}\n")
     
     if new_listings:
         for car in new_listings:
             if car not in previous:
-                print(f"Sending WhatsApp message for car: {car}")
-                send_whatsapp_message(car)
+                print(f"Sending message for car: {car}")
+                send_message(car)
     
     print("Returning current listings")
     return current
