a131e873d815ec463461cc2632cb89fa274af1ce
omararra <omarsamirelmokadem@gmail.com>
2024-11-10 12:18:49 +0300

    updated writing to file

--- function_app.py
+++ function_app.py
@@ -9,28 +9,25 @@ car_listings = ext.get_car_listings(1)
 with open("cars.txt", "w") as f:
     for car in car_listings:
         f.write(f"{car}\n")
-@app.schedule(schedule="* * * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False)
-#@app.queue_output(arg_name="msg", queue_name="outqueue", connection="AzureWebJobsStorage")
+@app.schedule(schedule="*/30 * * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False)
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
     logging.info("Fetching current car listings...")
     car_listings = ext.get_car_listings(1)
-    # def convert_type(value): 
-    #     try: 
-    #         return eval(value) 
-    #     except: 
-    #         return value 
     previous = []
     with open("cars.txt", "r") as file: 
         for line in file:
             previous.append(eval(line.strip()))
+    with open("cars.txt", "w") as f:
+        f.write("")
     if car_listings == previous:
         logging.info("No new cars")
     else:
         current = qs.main(car_listings, previous)
         for car in current:
-            with open("cars.txt", "w") as f:
+            with open("cars.txt", "a") as f:
                 f.write(f"{car}\n")
     logging.info("Exiting main function")
     logging.info('Python timer trigger function executed.')
+    logging.info("\n\n")
--- qatarsale.py
+++ qatarsale.py
@@ -26,31 +26,31 @@ def send_message(car_details):
         print("Discord message sent successfully.")
     else:
         print(f"Failed to send message. Status code: {response.status_code}")
-    print("Sending Email message...")
-    message = Mail(
-        from_email='info@oryxdev.tech',
-        to_emails='omarsamirelmokadem@gmail.com',
-        subject='New Listing',
-        html_content=(
-            f"New car listing:<br>"
-            f"<strong>Make:</strong> {car_details[0]}<br>"
-            f"<strong>Model:</strong> {car_details[1]}<br>"
-            f"<strong>Submodel:</strong> {car_details[2]}<br>"
-            f"<strong>Price:</strong> {car_details[3]} QAR<br>"
-            f"<strong>Type:</strong> {car_details[4]}<br>"
-            f"<strong>Year:</strong> {car_details[5]}<br>"
-            f"<strong>Gear Type:</strong> {car_details[6]}<br>"
-            f"<strong>Cylinders:</strong> {car_details[7]}<br>"
-            f"<strong>Mileage:</strong> {car_details[8]}<br>"
-            f"<strong>Link:</strong> <a href='{car_details[9]}'>Click Here</a><br>"
-        ))
-    try:
-        sg = SendGridAPIClient("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")
-        response = sg.send(message)
-        print(response.status_code)
-        print(response.body)
-    except Exception as e:
-        print(e.message)
+    # print("Sending Email message...")
+    # message = Mail(
+    #     from_email='info@oryxdev.tech',
+    #     to_emails='fortnitedarxdd@gmail.com',
+    #     subject='New Listing',
+    #     html_content=(
+    #         f"New car listing:<br>"
+    #         f"<strong>Make:</strong> {car_details[0]}<br>"
+    #         f"<strong>Model:</strong> {car_details[1]}<br>"
+    #         f"<strong>Submodel:</strong> {car_details[2]}<br>"
+    #         f"<strong>Price:</strong> {car_details[3]} QAR<br>"
+    #         f"<strong>Type:</strong> {car_details[4]}<br>"
+    #         f"<strong>Year:</strong> {car_details[5]}<br>"
+    #         f"<strong>Gear Type:</strong> {car_details[6]}<br>"
+    #         f"<strong>Cylinders:</strong> {car_details[7]}<br>"
+    #         f"<strong>Mileage:</strong> {car_details[8]}<br>"
+    #         f"<strong>Link:</strong> <a href='{car_details[9]}'>Click Here</a><br>"
+    #     ))
+    # try:
+    #     sg = SendGridAPIClient("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")
+    #     response = sg.send(message)
+    #     print(response.status_code)
+    #     print(response.body)
+    # except Exception as e:
+    #     print(e.message)
     # print("Sending WhatsApp message...")
     # account_sid = 'ACcc19f7f4b5e9972e26f19afc773a076a'
     # auth_token = '9c901fee2bb955dd749acc740f2466d2'
